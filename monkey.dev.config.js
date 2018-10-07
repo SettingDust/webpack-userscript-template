54369411362abe4fbf3f91627054a3480ccd7664
SettingDust <settingdust@gmail.com>
2018-10-07 18:45:18 +0800

    not /

--- monkey.dev.config.js
+++ monkey.dev.config.js
@@ -1,6 +1,6 @@
 const monkey = require("./monkey.config");
 
 //The test path.
-monkey.require.push(__dirname + "/test/" + monkey.name.toLowerCase().replace(" ", "-") + ".js");
+monkey.require.push(__dirname + "\\test\\" + monkey.name.toLowerCase().replace(" ", "-") + ".js");
 
 module.exports = monkey;
