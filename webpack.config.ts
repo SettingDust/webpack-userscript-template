44fffef048026c0b7dbb7d24183851c6fb24fde4
SettingDust <settingdust@gmail.com>
2020-06-14 10:33:10 +0800

    :tada: Init typescript

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,5 +1,14 @@
 import { Configuration } from 'webpack';
 
 export default {
-
+    entry: 'index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    }
 } as Configuration;
